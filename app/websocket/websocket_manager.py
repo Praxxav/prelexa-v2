885ea816a989322b83f75004f5ebc4eb0e9ecd8b
Praxxav <pranavkulkarni33917@gmail.com>
2026-01-21 21:40:03 +0530

    ws meeting fixed

--- app/websocket/websocket_manager.py
+++ app/websocket/websocket_manager.py
@@ -6,12 +6,12 @@ class ConnectionManager:
         # Map: room_id -> List[{ "ws": WebSocket, "user_id": str }]
         self.active_rooms: Dict[str, List[Dict]] = {}
 
-    async def connect(self, room_id: str, user_id: str, websocket: WebSocket):
+    async def connect(self, room_id: str, user_id: str, websocket: WebSocket, user_name: str = "Anonymous"):
         await websocket.accept()
         if room_id not in self.active_rooms:
             self.active_rooms[room_id] = []
         
-        self.active_rooms[room_id].append({"ws": websocket, "user_id": user_id})
+        self.active_rooms[room_id].append({"ws": websocket, "user_id": user_id, "user_name": user_name})
 
     def disconnect(self, room_id: str, websocket: WebSocket):
         if room_id in self.active_rooms:
@@ -46,4 +46,13 @@ class ConnectionManager:
                     except:
                         pass
                     return
+
+    def get_participants(self, room_id: str) -> List[Dict]:
+        if room_id in self.active_rooms:
+            return [
+                {"userId": c["user_id"], "userName": c.get("user_name", "Anonymous")}
+                for c in self.active_rooms[room_id]
+            ]
+        return []
+
 connection_manager = ConnectionManager()
